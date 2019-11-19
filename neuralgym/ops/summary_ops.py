15451f07314f5d87b9291b38ef769873ad068875
Nels Salminen <n.salminen@me.com>
2019-11-19 07:28:59 +0100

    Add extra color modes

--- neuralgym/ops/summary_ops.py
+++ neuralgym/ops/summary_ops.py
@@ -107,21 +107,26 @@ def filters_summary(kernel, rescale=True, name='kernel'):
 def images_summary(images, name, max_outs, color_format='BGR'):
     """Summary images.
 
-    **Note** that images should be scaled to [-1, 1] for 'RGB' or 'BGR',
-    [0, 1] for 'GREY'.
+    **Note** that images should be scaled to [-1, 1] for 'RGB', 'BGR', 'BGRA' and 'RGBA',
+    and [0, 1] for 'GREY'.
 
     :param images: images tensor (in NHWC format)
     :param name: name of images summary
     :param max_outs: max_outputs for images summary
-    :param color_format: 'BGR', 'RGB' or 'GREY'
+    :param color_format: 'BGR', 'BGRA', 'RGB', 'RGBA', or 'GREY'
     :return: None
     """
     with tf.variable_scope(name), tf.device('/cpu:0'):
         if color_format == 'BGR':
             img = tf.clip_by_value(
                 (tf.reverse(images, [-1])+1.)*127.5, 0., 255.)
+        elif color_format == 'ABGR':
+            img = tf.clip_by_value(
+                (tf.reverse(images, [-1]) + 1.) * 127.5, 0., 255.)
         elif color_format == 'RGB':
             img = tf.clip_by_value((images+1.)*127.5, 0, 255)
+        elif color_format == 'RGBA':
+            img = tf.clip_by_value((images + 1.) * 127.5, 0, 255)
         elif color_format == 'GREY':
             img = tf.clip_by_value(images*255., 0, 255)
         else:
